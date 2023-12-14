8bd2c86a0f19847560b416e5a8a28412fd408a8e
Mario <NA>
2023-12-14 12:37:26 -0600

    first commit

---
+++ RacerScene.cs
@@ -0,0 +1,148 @@
+//============================================================================
+// RacerScene
+//============================================================================
+using Godot;
+using System;
+using System.Linq.Expressions;
+
+public partial class RacerScene : Node3D
+{
+	private CamRig cam;
+	float longitudeDeg;
+	float latitudeDeg;
+	float camDist;
+	float camFOV;
+	Vector3 camTg;       // coords of camera target
+	Vector3 camSubject;  // coords that cam is lerping toward
+	float lerpSpeed;     // factor for lerping the camera
+
+	Cart cart;           // model of the cart
+	double wheelRad;     // wheel radius
+	double wheelRadS;    // wheel radius of steared wheel
+	double wheelSep;     // distance separating the two wheels.
+	double baseLen;      // base length, from rear wheel axle to steer axis.
+	double casterLen;    // length of caster
+	double cgDist;       // longit distance from wheel axle to CG
+	float tireWd;        // tire width
+	float wheelWd;      // wheel width
+	float tireWdS;      // thickness of steered wheel
+	float wheelWdS;     // width of steered wheel
+	float frameThk;      // frame thickness
+	double m;            // mass of racer
+	double rGyr;         // radius of gyration
+
+	double steerSig;     // steer signal from pilot
+
+	RollerRacer racer;   // simulation of the Roller Racer
+	double time;         // elapsed time
+
+	UIPanelDisplay dataDisplay;
+
+	//------------------------------------------------------------------------
+	// _Ready: Called when the node enters the scene tree for the first time.
+	//------------------------------------------------------------------------
+	public override void _Ready()
+	{
+		// Set up the camera rig
+		longitudeDeg = 30.0f;
+		latitudeDeg = 20.0f;
+		camDist = 4.0f;
+		camFOV = 55.0f;
+
+		lerpSpeed = 3.0f;
+		camSubject = new Vector3(0.0f, 1.0f, 0.0f);
+		camTg = new Vector3(0.0f, 1.0f, 0.0f);
+		cam = GetNode<CamRig>("CamRig");
+		cam.LongitudeDeg = longitudeDeg;
+		cam.LatitudeDeg = latitudeDeg;
+		cam.Distance = camDist;
+		cam.Target = camTg;
+		cam.FOVDeg = camFOV;
+
+		// set up the data display
+		dataDisplay = GetNode<UIPanelDisplay>("Control/MarginContainer/DataDisplay");
+		dataDisplay.SetNDisplay(5);
+		dataDisplay.SetLabel(0,"Roller Racer");
+		dataDisplay.SetValue(0,"");
+		dataDisplay.SetLabel(1,"Speed");
+		dataDisplay.SetValue(1,"---");
+		dataDisplay.SetLabel(2,"Kin. Energy");
+		dataDisplay.SetValue(2,"---");
+		dataDisplay.SetLabel(3,"Slip Rate F");
+		dataDisplay.SetValue(3,"---");
+		dataDisplay.SetLabel(4,"Slip Rate R");
+		dataDisplay.SetValue(4,"---");
+
+		// set up the cart model
+		cart = GetNode<Cart>("Cart");
+		wheelRad = 0.5 * 0.75;  cart.WheelRadius = (float)wheelRad;
+		wheelRadS = 0.15;       cart.SteeredWheelRadius = (float)wheelRadS;
+		wheelSep = 1.0;         cart.WheelSeparation = (float)wheelSep;
+		baseLen = 1.3;			cart.BaseLength = (float)baseLen;
+		casterLen = 0.3; 		cart.CasterLength = (float)casterLen;
+		cgDist = 0.6;
+		tireWd = 0.08f;         cart.TireWidth = tireWd;
+		wheelWd = 0.5f * tireWd; 	cart.WheelWidth = wheelWd;
+		tireWdS = 0.05f;		cart.SteeredTireWidth = tireWdS;
+		wheelWdS = 0.7f * tireWdS;	cart.SteeredWheelWidth = wheelWdS;
+		frameThk = 0.08f;		cart.FrameBarWidth = frameThk;
+		cart.BuildModel();
+
+		// set up the simulation
+		m = 25.0;
+		rGyr = 0.3;
+		racer = new RollerRacer();    // simulation
+		time = 0.0;
+	}
+
+	//------------------------------------------------------------------------
+	// _Process
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	//------------------------------------------------------------------------
+	public override void _Process(double delta)
+	{
+		
+		cart.SetLoc(0.0f, 0.0f, 0.0f, // need to update this with other states
+			0.0f, 0.0f, 0.0f, 
+			(float)racer.SteerAngle);
+	}
+
+    //------------------------------------------------------------------------
+    // _PhysicsProcess
+    //------------------------------------------------------------------------
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+		ProcessPilotInput();
+		racer.SteerAngleSignal = (-50.0 * steerSig)*Math.PI/180.0;
+
+		racer.StepRK2(time,delta);  // You are going to use the RK4 integrator
+		time += delta;
+
+		camSubject.X = (float)racer.xG;
+		camSubject.Z = (float)racer.zG;
+		camTg = camTg.Lerp(camSubject, lerpSpeed*(float)delta);
+		cam.Target = camTg;
+    }
+
+    //------------------------------------------------------------------------
+    // ProcessPilotInput:
+    //------------------------------------------------------------------------
+    private void ProcessPilotInput()
+	{
+
+		steerSig = (double)Input.GetJoyAxis(0, JoyAxis.LeftX);
+		if(Math.Abs(steerSig) < 0.01)
+			steerSig = 0.0;
+
+		if(Input.IsActionPressed("ui_left")){
+			steerSig = -1.0;
+		}
+		if(Input.IsActionPressed("ui_right")){
+			steerSig = 1.0;
+		}
+
+	}
+
+}
---
+++ Sims/RollerRacer.cs
@@ -0,0 +1,254 @@
+//============================================================================
+// RollerRacer.cs : Defines derived class for simulating a Roller Racer.
+//       Equations of motion are derived in class notes.
+//============================================================================
+using System;
+
+public class RollerRacer : Simulator
+{
+    // physical parameters, names are the same as that in the notes
+    double m;   // mass of vehicle
+    double Ig;  // moment of inertia (vertical axis) about center of mass
+    double b;   // distance of com ahead of rear axle
+    double c;   // distance of rear contact patch from symmetry axis
+    double d;   // caster length
+    double h;   // longitudinal distance between center of mass and steer axis
+
+    double rW;  // radius of rear wheel, used for calculating rotation rate
+    double rWs; // radius of steered wheel, used for calculating rotation rate
+
+    double kPDelta;  // proportional gain for steer filter
+    double kDDelta;  // derivative gain for steer filter
+    double deltaDes; // desired steer angle
+
+    double muS;      // static frict coeff, lower bound
+
+    bool simBegun;   // indicates whether simulation has begun
+
+    public RollerRacer() : base(11)
+    {
+        g = 9.81;
+        muS = 0.9;
+        SetInertia(25.0 /*mass*/, 0.3 /*radius of gyration*/);
+        SetGeometry(1.3 /*wheel base*/, 0.6 /* cg dist from axle*/,
+            0.3 /*caster dist*/, 1.0 /*wheel sep*/, 0.5*0.75 /*Rwheel radius*/,
+            0.15 /*steered wheel radius*/);
+        kPDelta = 10.0;
+        kDDelta = 4.0;
+
+        x[0] = 0.0;   // x coordinate of center of mass
+        x[1] = 0.0;   // xDot, time derivative of x
+        x[2] = 0.0;   // z coordinate of center of mass
+        x[3] = 0.0;   // zDot, time derivative of z
+        x[4] = 0.0;   // psi, heading angle
+        x[5] = 0.0;   // psiDot, time derivative of heading, yaw rate
+        x[6] = 0.0;   // rotation angle of left rear wheel
+        x[7] = 0.0;   // rotation angle of right rear wheel
+        x[8] = 0.0;   // rotation angle of front steered wheel
+        x[9] = 0.0;   // delta, steer angle
+        x[10] = 0.0;  // deltaDot, steer rate
+
+        SetRHSFunc(RHSFuncRRacer);
+        simBegun = false;
+    }
+
+    private void RHSFuncRRacer(double[] xx, double t, double[] ff)
+    {
+        // give names to some state variable so code is easier to read & write
+        double xDot = xx[1];
+        double zDot = xx[3];
+        double psi  = xx[4];
+        double psiDot = xx[5];
+        double delta = xx[9];
+        double deltaDot = xx[10];
+
+        // calculate some trig functions here, so you only have to do it once
+        double cosPsi = Math.Cos(psi);
+        double sinPsi = Math.Sin(psi);
+        double cosDelta = Math.Cos(delta);
+        double sinDelta = Math.Sin(delta);
+        double cosPsiPlusDelta = Math.Cos(psi + delta);
+        double sinPsiPlusDelta = Math.Sin(psi + delta);
+
+        // #### You will do some hefty calculations here
+
+        // #### Right sides are zero for now. You will fix
+        ff[0] = 0.0;
+        ff[1] = 0.0;
+        ff[2] = 0.0;
+        ff[3] = 0.0;
+        ff[4] = 0.0;
+        ff[5] = 0.0;
+        ff[6] = 0.0;
+        ff[7] = 0.0;
+        ff[8] = 0.0;
+        ff[9] = deltaDot;
+        ff[10] = -kDDelta*deltaDot -kPDelta*(delta - deltaDes);
+
+        simBegun = true;
+    }
+
+    //------------------------------------------------------------------------
+    // SetInitialSpeed: Sets the initial speed of the vehicle. Must be set
+    //          before simulation has begun.
+    //------------------------------------------------------------------------
+    public void SetInitalSpeed(double val)
+    {
+        if(simBegun) return;
+
+        x[1] = val;
+    }
+
+    //------------------------------------------------------------------------
+    // SetInertia: sets the two inertia properties of the vehicle. 
+    //     mm: total mass in kilograms
+    //     rgyr: radius of gyration in meters
+    //------------------------------------------------------------------------
+    public void SetInertia(double mm, double rgyr)
+    {
+        if(mm <= 0.1)   // check lower bound for mass
+            return;     // return and not update parameters.
+
+        if(rgyr < 0.03) // check lower bound for radius of gyration
+            return;     // return and not update parameters.
+
+        m = mm;
+        Ig = m*rgyr*rgyr;
+    }
+
+    //------------------------------------------------------------------------
+    // SetGeometry: Sets the geometry of the vehicle.
+    //    wsb: distance between rear axle and steer axis
+    //    dcg: distance from wheel axle to center of mass
+    //    dcst: length of the caster
+    //    wid: distance between rear wheels
+    //    wRad: radius of rear wheel
+    //    wRadS: radius of steered wheel
+    //------------------------------------------------------------------------
+    public void SetGeometry(double wsb, double dcg, double dcst, double wid, 
+        double wRad, double wRadS)
+    {
+        // check lower bounds
+        if(wsb < 0.01) return;
+        if(dcg <= 0.0) return;
+        if(dcst < 0.0) return;
+        if(wid < 0.05) return;
+        if(wRad < 0.05) return;
+        if(wRadS < 0.05) return;
+
+        if(wsb-dcst < dcg) return; //cg must be btw rear axle and steer contact
+
+        b = dcg;
+        c = 0.5*wid;
+        d = dcst;
+        h = wsb-dcg;
+
+        rW = wRad;
+        rWs = wRadS;
+    }
+
+    //------------------------------------------------------------------------
+    // Getters/Setters
+    //------------------------------------------------------------------------
+
+    public double SteerAngleSignal
+    {
+        set{
+            deltaDes = value;
+        }
+    }
+
+    public double SteerAngle
+    {
+        get{
+            return x[9];
+        }
+    }
+
+    public double xG
+    {
+        get{
+            return x[0];
+        }
+    }
+
+    public double zG
+    {
+        get{
+            return x[2];
+        }
+    }
+
+    public double Heading
+    {
+        get{
+            return x[4];
+        }
+    }
+
+    public double WheelAngleL
+    {
+        get{
+            return x[6];
+        }
+    }
+
+    public double WheelAngleR
+    {
+        get{
+            return x[7];
+        }
+    }
+
+    public double WheelAngleF
+    {
+        get{
+            return x[8];
+        }
+    }
+
+    public double Speed
+    {
+        get{
+            // ######## You have to write this part ################
+
+            return(-1.21212121);
+        }
+    }
+
+    public double KineticEnergy
+    {
+        get{
+            // ######## You have to write this part ################
+
+            return(-1.21212121);
+        }
+    }
+
+    public double SlipRateFront
+    {
+        get{
+            // ######## You have to write this part ################
+
+            return(-1.21212121);
+        }
+    }
+
+    public double SlipRateRear
+    {
+        get{
+            // ######## You have to write this part ################
+
+            return(-1.21212121);
+        }
+    }
+
+    public double FontFrictionFactor
+    {
+        get{
+            // ######## You have to write this part ################
+
+            return(-1.21212121);
+        }
+    }
+}
